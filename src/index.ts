c09ccd72eb4a2d0abe94a281c5e4bb4258901f0d
hhimanshu <dev.atwork@yahoo.com>
2023-10-26 17:33:41 -0700

    run complete example

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,38 @@
-const { OpenAI } = require("langchain/llms/openai");
-const { ChatOpenAI } = require("langchain/chat_models/openai");
+import { OpenAI } from "langchain/llms/openai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanMessage } from "langchain/schema";
+import { BaseOutputParser } from "langchain/schema/output_parser";
+import { ChatPromptTemplate, PromptTemplate } from "langchain/prompts";
 
 require("dotenv").config();
 
-console.log(`The API key is ${process.env.OPENAI_API_KEY}`);
-
-const chatModel = new ChatOpenAI();
+/**
+ * Parse the output of an LLM call to a comma-separated list.
+ */
+class CommaSeparatedListOutputParser extends BaseOutputParser<string[]> {
+  async parse(text: string): Promise<string[]> {
+    return text.split(",").map((item) => item.trim());
+  }
+}
 
 const run = async () => {
-  const llm = new OpenAI({
-    temperature: 0.9,
-  });
+  const template = `You are a helpful assistant who generates comma separated lists.
+A user will pass in a category, and you should generate 5 objects in that category in a comma separated list.
+ONLY return a comma separated list, and nothing more.`;
+  const humanTemplate = "{text}";
 
-  const text =
-    "What would be a good company name for a company that nutrition labels";
-  const llmResult = await llm.predict(text);
-  const chatModelResult = await chatModel.predict(text);
+  const chatPrompt = ChatPromptTemplate.fromMessages([
+    ["system", template],
+    ["human", humanTemplate],
+  ]);
 
-  console.log("LLM Result: ", llmResult);
-  console.log("Chat Model Result: ", JSON.stringify(chatModelResult, null, 2));
+  const chatModel = new ChatOpenAI();
+  const parser = new CommaSeparatedListOutputParser();
+  const chain = chatPrompt.pipe(chatModel).pipe(parser);
+  const result = await chain.invoke({
+    "text": "fortune 500 companies"
+  })
+  console.log("Result: ", result);
 };
 
 run().then(() => console.log("Done!"));
